297ac5ab6689f6ca9bf1231629b3c0637b89796d
dhanush <dhanushdev201@gmail.com>
2023-03-04 18:46:20 +0530

    first duck

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>this project has to be modified ,and ya this was started by dhanush</h1>
+      <img src="https://wallpapercave.com/uwp/uwp1370413.jpeg" alt=""/>
     </div>
   );
 }
